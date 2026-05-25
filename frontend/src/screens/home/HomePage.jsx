832e3c5fe2bad4c7c65815c7e3d4425f4b0e1095
Luisgzanetti <luisguilhermezanetti@gmail.com>
2026-05-25 18:03:07 -0300

    Filtro de tarefas implementado

--- frontend/src/screens/home/HomePage.jsx
+++ frontend/src/screens/home/HomePage.jsx
@@ -5,8 +5,7 @@ import SideBar from "../../components/SideBar/SideBar.jsx";
 import SearchBar from "../../components/SearchBar/SearchBar.jsx";
 import TaskCard from '../../components/TaskCard/TaskCard.jsx';
 import DeleteConfirmCard from '../../components/DeleteConfirmCard/DeleteConfirmCard.jsx';
-import Notification from "../notifications/noti.jsx"
-import lixo from "../lixo/lixo.jsx"
+import FilterCard from '../../components/FilterCard/FilterCard.jsx';
 import { useState } from "react"
 import { useApp } from '../../context/AppContext.jsx';
 
@@ -15,6 +14,10 @@ export default function HomePage({ setPagina }) {
     const { tasks, setTasks } = useApp()
     const [search, setSearch] = useState("")
     const [deleteTaskId, setDeleteTaskId] = useState(null)
+    const [showFilterOverlay, setShowFilterOverlay] = useState(false)
+    const [showOverdue, setShowOverdue] = useState(true)
+    const [showOngoing, setShowOngoing] = useState(true)
+    const [showCompleted, setShowCompleted] = useState(true)
 
     function handleChangeSearch(event) {
         setSearch(event.target.value)
@@ -53,39 +56,46 @@ export default function HomePage({ setPagina }) {
             </div>
             <div className='total-tasks-display'>
                 <p className='total-tasks-text'>Minhas Tarefas ({tasks.length})</p>
-                <BiSortAlt2 size={34} />
+                <BiSortAlt2 size={34} onClick={() => setShowFilterOverlay(true)} />
             </div>
             <div className='tasks-display'>
-                <div className='category-tasks'>
-                    <p className='category-title'>Atrasadas ({tasks.filter(task => task.category === 'Atrasada').length})</p>
-                    {filteredTasks.map(task => (
-                        task.category === 'Atrasada' && (
-                            <TaskCard task={task} key={task.id} completeTask={completeTask} deleteTask={setDeleteTaskId} />
-                        )
-                    ))}
-                </div>
-                <div className='category-tasks'>
-                    <p className='category-title'>Em Andamento ({tasks.filter(task => task.category === 'Em progresso').length})</p>
-                    {filteredTasks.map(task => (
-                        task.category === 'Em progresso' && (
-                            <TaskCard task={task} key={task.id} completeTask={completeTask} deleteTask={setDeleteTaskId} />
-                        )
-                    ))}
-                </div>
-                <div className='category-tasks'>
-                    <p className='category-title'>Concluídas ({tasks.filter(task => task.category === 'Concluída').length})</p>
-                    {filteredTasks.map(task => (
-                        task.category === 'Concluída' && (
-                            <TaskCard task={task} key={task.id} completeTask={completeTask} deleteTask={setDeleteTaskId} />
-                        )
-                    ))}
-                </div>
+                {showOverdue && (
+                    <div className='category-tasks'>
+                        <p className='category-title'>Atrasadas ({tasks.filter(task => task.category === 'Atrasada').length})</p>
+                        {filteredTasks.map(task => (
+                            task.category === 'Atrasada' && (
+                                <TaskCard task={task} key={task.id} completeTask={completeTask} deleteTask={setDeleteTaskId} />
+                            )
+                        ))}
+                    </div>
+                )}
+                {showOngoing && (
+                    <div className='category-tasks'>
+                        <p className='category-title'>Em Andamento ({tasks.filter(task => task.category === 'Em progresso').length})</p>
+                        {filteredTasks.map(task => (
+                            task.category === 'Em progresso' && (
+                                <TaskCard task={task} key={task.id} completeTask={completeTask} deleteTask={setDeleteTaskId} />
+                            )
+                        ))}
+                    </div>
+                )}
+                {showCompleted && (
+                    <div className='category-tasks'>
+                        <p className='category-title'>Concluídas ({tasks.filter(task => task.category === 'Concluída').length})</p>
+                        {filteredTasks.map(task => (
+                            task.category === 'Concluída' && (
+                                <TaskCard task={task} key={task.id} completeTask={completeTask} deleteTask={setDeleteTaskId} />
+                            )
+                        ))}
+                    </div>
+                )}
             </div>
             <SideBar
                 paginaAtual="home"
                 setPagina={setPagina}
             />
             {deleteTaskId && <DeleteConfirmCard deleteId={deleteTaskId} deleteTask={deleteTask} setDeleteId={setDeleteTaskId} />}
+            {showFilterOverlay && <FilterCard setShowFilterOverlay={setShowFilterOverlay} showOverdue={showOverdue} setShowOverdue={setShowOverdue} showOngoing={showOngoing} setShowOngoing={setShowOngoing} showCompleted={showCompleted} setShowCompleted={setShowCompleted} />}
         </div>
     )
 }
